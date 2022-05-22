71a5797a4bb37b31290c57e5efe59b7856fafe01
Stephen <55069019+S709394@users.noreply.github.com>
2022-05-22 14:19:37 -0400

    Create 53_leetcode_try1.cpp
    The normal version, can be shorter.

---
+++ Leetcode_53/src/com/leetcode/53_leetcode_try1.cpp
@@ -0,0 +1,12 @@
+//CPP version
+int maxProfit(vector<int>& prices) {
+  int res =0;
+  for (int i=1 ;i<prices.size(); i++){
+    //version 1
+    if(prices[i]>prices[i-1])
+    {
+      res+= prices [i] - prices[i-1];
+    }
+  }
+  return res;
+}
