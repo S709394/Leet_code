738aab154e3b846292aff846be003fc5d647c370
Stephen <55069019+S709394@users.noreply.github.com>
2022-06-18 23:12:15 -0400

    Add files via upload
    Path Sum II  113

---
+++ Leetcode_113/src/com/leetcode/Leetcode_113_try1.java
@@ -0,0 +1,89 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*113. Path Sum II
+Medium
+
+4874
+
+109
+
+Add to List
+
+Share
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Explanation: There are two paths whose sum equals targetSum:
+5 + 4 + 11 + 2 = 22
+5 + 8 + 4 + 5 = 22
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+Accepted
+589,116
+Submissions
+1,083,811
+ * */
+
+
+/*Idea: does not fully understand so far ,  70% finished
+ * 
+ *   https://leetcode.com/problems/path-sum-ii/discuss/36683/DFS-with-one-LinkedList-accepted-java-solution
+ * */ 
+
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode_113_try1 {
+	public class TreeNode {
+		     int val;
+		     TreeNode left;
+		     TreeNode right;
+		     TreeNode() {}
+		      TreeNode(int val) { this.val = val; }
+		      TreeNode(int val, TreeNode left, TreeNode right) {
+		          this.val = val;
+		          this.left = left;
+		          this.right = right;
+		      }
+		  }
+		 
+	
+}
