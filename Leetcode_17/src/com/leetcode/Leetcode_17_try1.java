b5975aaf97da054700df9d341b14b2590b766904
Stephen <55069019+S709394@users.noreply.github.com>
2022-06-22 02:36:23 -0400

    Add files via upload

--- Leetcode_17/src/com/leetcode/Leetcode_17_try1.java
+++ Leetcode_17/src/com/leetcode/Leetcode_17_try1.java
@@ -36,74 +36,85 @@ digits[i] is a digit in the range ['2', '9'].
  * 
  * */package com.leetcode;
 
+ 
+ /*Idea   backtrack  
+  * 
+  *   Does not works so far   meet me at try 2  
+  * */
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+
+
 
 public class Leetcode_17_try1 {
 	
 	
 //	List<String> ans =new ArrayList<>();
-	
+	static Map<Character, String> NUMS = new HashMap<Character, String>() {
+		{
+		put('0', "");
+        put('1', "");
+        put('2', "abc");
+        put('3', "def");
+        put('4', "ghi");
+        put('5', "jkl");
+        put('6', "mno");
+        put('7', "pqrs");
+        put('8', "tuv");
+        put('9', "wxyz");
+        }
+	};
 	
 	
 	public static void main(String[] args) {
-		  Map<Character, String> NUMS = new HashMap() {
-			{
-			put('0', "");
-	        put('1', "");
-	        put('2', "abc");
-	        put('3', "def");
-	        put('4', "ghi");
-	        put('5', "jkl");
-	        put('6', "mno");
-	        put('7', "pqrs");
-	        put('8', "tuv");
-	        put('9', "wxyz");
-	        }
-		};
+		
 		
 		String str = "23";
 		
 		String[]  keyPad = new String []  {"" ,"","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 		
 		
-		public List<String> letterCobinations (String digits){
-			if(digits.length()==0) {
-				return new ArrayList<>();
-			}
-			
-			StringBuilder sb =new StringBuilder();
-			List<String> res = new ArrayList();
-			
-			dfs (sb ,0,digits,NUMS,res);
-				
-			
-			///
-			
-			return res;
+		
+		
+		
+	}
+	
+	public static List<String> letterCobinations (String digits){
+		if(digits.length()==0) {
+			return new ArrayList<>();
 		}
-		public void dfs(StringBuilder sb , int index,String digits ,Map<Character, String> nums,List<String> res) {
-			//the end of the digits
-			if(index ==digits.length()) {
-				res.add(sb.toString());
-				System.out.println("res.add(sb.toString());");
-				return;
-			}
+		
+		StringBuilder sb =new StringBuilder();
+		List<String> res = new ArrayList();
+		
+		dfs (sb ,0,digits,NUMS,res);
 			
-			char digit =digits.charAt(index);
-			String options =nums .getOrDefault(digit, digits+"");
+		
+		///
+		
+		return res;
+	}
+	public static  void dfs(StringBuilder sb , int index,String digits ,Map<Character, String> nums,List<String> res) {
+		//the end of the digits
+		if(index ==digits.length()) {
+			res.add(sb.toString());
+			System.out.println("res.add(sb.toString());");
+			return;
+		}
+		
+		char digit =digits.charAt(index);
+		String options =nums .getOrDefault(digit, digits+"");
+		
+		for (char option :options.toCharArray()) {
+			sb.append(option);
+			dfs(sb,index+1,digits,nums,res);
 			
-			for (char option :options.toCharArray()) {
-				sb.append(option);
-				dfs(sb,index+1,digits,nums,res);
-				
-				System.out.println( "sb " + sb); //test
-				sb.deleteCharAt(sb.length()-1);
-				System.out.println( "sb (deleted)" + sb); //test
-				
-			}
-			
+			System.out.println( "sb " + sb); //test
+			sb.deleteCharAt(sb.length()-1);
+			System.out.println( "sb (deleted)" + sb); //test
 			
 		}
 		
---
+++ Leetcode_17/src/com/leetcode/Leetcode_17_try2.java
@@ -0,0 +1,105 @@
+/*17. Letter Combinations of a Phone Number
+Medium
+
+11016
+
+710
+
+Add to List
+
+Share
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+ 
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+
+Constraints:
+
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+ * 
+ * */package com.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*Idea    From  : https://leetcode.com/problems/letter-combinations-of-a-phone-number/discuss/8064/My-java-solution-with-FIFO-queue
+  *  LinedList
+  *  
+  *  Character.getNumericValue
+  *  
+  *  LinkedList.peek(); // "Looking the 1st elem  but not remove it "
+  *   
+  * */
+
+
+
+public class Leetcode_17_try2 {
+	public static void main(String[] args) {
+		String digits = "786";
+		
+//		String digits = "239";
+		
+		
+		letterCombinations(digits);
+	}
+	
+	static  List<String> letterCombinations(String digits) {
+		
+		//For Input: digits = "" ;
+//		List <String> LS= new LinkedList<String>();  // wrong
+		LinkedList <String> LS= new LinkedList<String>();  
+		if(digits.length()==0) {
+			return LS;
+		}
+		
+		String [] mapping = new String [] { 
+			 "0","1","abc", "def" , "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+		};
+		//??? no idea
+		LS.add("");
+		
+		for (int i = 0 ; i < digits.length();i++) {
+			
+			//set the x to the number of the one user press
+			int x =Character.getNumericValue(digits.charAt(i));
+			
+			System.out.println("int x : " + x);//test
+			System.out.println("LS.peek().length():" +LS.peek().length());//test
+			
+			while (LS.peek().length()==i) {
+//				System.out.println("LS .size() (before :"+LS.size());//test
+					String t = LS.remove();
+					
+					System.out.println("String t :"+t);//test
+					System.out.println("LS .size()  (after:"+LS.size());//test
+				
+				for(char s : mapping[x].toCharArray()) {
+					
+//					System.out.println(s);//test
+					LS.add(t+s);
+//					System.out.println("t+s:" + t+ s);//test
+				}
+				
+			}
+		}
+		return LS;
+		
+	}
+}
