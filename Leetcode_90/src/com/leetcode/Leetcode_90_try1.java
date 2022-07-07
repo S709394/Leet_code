effb375f4e4fae3123d1180535a822ddd65aafbf
Stephen <55069019+S709394@users.noreply.github.com>
2022-07-07 02:59:35 -0400

    Add files via upload

---
+++ Leetcode_90/src/com/leetcode/Leetcode_90_try1.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+public class Leetcode_90_try1 {
+	public static void main(String[] args) {
+		int [] nums= {1,2,2};
+		System.out.println( subsetsWithDup (nums));
+	}
+	
+	//DFS    
+	
+	
+}
