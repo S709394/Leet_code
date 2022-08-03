3e9764a7002f07c25998c617bd640503365e1c8f
Stephen <55069019+S709394@users.noreply.github.com>
2022-08-02 22:37:29 -0400

    Add files via upload

--- Leetcode_98/src/com/leetcode/Leetcode_try4_practise2.java
+++ Leetcode_98/src/com/leetcode/Leetcode_try4_practise2.java
@@ -1,5 +1,65 @@
+/*Leetcode 98
+ * 
+ * */
+
+/* Idea: 
+ * 
+ * 
+ * */
+
 package com.leetcode;
 
-public class Leetcode_try4_practise2 {
+import java.util.Stack;
 
+import com.leetcode.Leetcode_98_try3_practise.TreeNode;
+
+
+
+public class Leetcode_try4_practise2 {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+	 public boolean isValidBST(TreeNode root) {
+		 Stack <TreeNode> stack = new Stack<TreeNode> ();
+			
+			
+			long  min_left_child= ( - Long.MAX_VALUE);
+			
+			while (root!=null || !stack.isEmpty()) {
+				
+				
+				//put the lefts till the   inOrder pos 0(very
+				while (root!=null) {
+					stack.push(root);
+					root=root.left;
+				}
+				
+				root = stack.pop();
+				if(root.val<=min_left_child) {
+					return false;
+				}
+				min_left_child = root.val;
+				
+				root= root.right;
+				
+			}
+			
+			return true;
+	      
+    }
 }
