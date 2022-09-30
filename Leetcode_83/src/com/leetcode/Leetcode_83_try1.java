0723b1208e3a1e1ae1f3af4d437144e273d4f3e0
Stephen <55069019+S709394@users.noreply.github.com>
2022-09-29 23:37:07 -0400

    using dummy to locate the head

---
+++ Leetcode_83/src/com/leetcode/Leetcode_83_try1.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+public class Leetcode_83_try1 {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public ListNode deleteDuplicates(ListNode head) {
+		// base case
+		if (head == null || head.next == null)
+			return head;
+
+		ListNode dummy = head;
+		while (dummy.next != null) {
+			if (dummy.val == dummy.next.val) {
+				dummy.next = dummy.next.next;
+			} else {
+				dummy = dummy.next;
+			}
+		}
+
+		return head;
+	}
+}
