dbba966cdb01c7b9ca0cb0736fab826778e32a30
Stephen <55069019+S709394@users.noreply.github.com>
2022-11-09 15:23:38 -0500

    Recursive

---
+++ Leetcode_1544/src/com/leetcode/Leetcode_1544_try1.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+public class Leetcode_1544_try1 {
+	public String makeGood(String s) {
+
+		for (int i = 0; i < s.length() - 1; i++) {
+
+			// 'a' - "A" ~ 'z' -"Z" are always abs(32 ) ;
+			if (Math.abs(s.charAt(i) - s.charAt(i + 1)) == 32) {
+
+				// remove the 2 char s then "+" them together
+				return makeGood(s.substring(0, i) + s.substring(i + 2));
+			}
+
+		}
+		return s;
+
+	}
+}
