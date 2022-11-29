3ca92bbba15d8c4a1061bb1bac1a07d23e7e544f
Stephen <55069019+S709394@users.noreply.github.com>
2022-11-29 14:52:28 -0500

    Practise try4

--- Leetcode_2195/src/com/leetcode/Leetcode_2195_try4_practise_set.java
+++ Leetcode_2195/src/com/leetcode/Leetcode_2195_try4_practise_set.java
@@ -1,5 +1,16 @@
 package com.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+//
+//Runtime
+//75 ms
+//Beats
+//41.72%
+//Memory
+//95.6 MB
+//Beats
+//26.30%
 public class Leetcode_2195_try4_practise_set {
 	public static void main(String[] args) {
 
@@ -23,7 +34,30 @@ public class Leetcode_2195_try4_practise_set {
 //	int k = 2;
 		System.out.println(minimalKSum(nums, k));
 	}
+
 	public static long minimalKSum(int[] nums, int k) {
-		// Sum  the k nums
+		Set <Integer> set = new HashSet <> () ;
+		
+		long res = (1L + k) * k /2 ;
+		int nextNum = k+1;
+		int cnt  = 0 ; 
+		
+		for (int num : nums) {
+			if (set.add(num) && k>=num) {
+				res-= num;
+				cnt ++;
+			}
+		}
+		
+		while (cnt >0) {
+			if (set.contains(nextNum)) {
+				nextNum++;
+			}
+			res+= nextNum;
+			nextNum ++;
+			
+			cnt--;
+		}
+		return res ;
 	}
 }
