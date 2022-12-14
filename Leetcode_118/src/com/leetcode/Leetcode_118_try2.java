3d3fae022267ffb607865ca03ddd0b77b099a41a
Stephen <55069019+S709394@users.noreply.github.com>
2022-12-13 22:34:02 -0500

    Try2

---
+++ Leetcode_118/src/com/leetcode/Leetcode_118_try2.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode_118_try2 {
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println(generate(n));
+	}
+
+	public static List<List<Integer>> generate(int n) {
+		List<List<Integer>> ll = new ArrayList<>();
+		// base case //
+		for (int i = 0; i < n ; i++) {
+			List<Integer> list = new ArrayList<Integer>();
+				
+			for (int j = 0; j < i+1; j++) {
+				if (i == 0 || j == i) {
+					list.add(1);
+				} else {
+					list.add(ll.get(i-1).get(j-1) + ll.get(i -1).get(j));
+				}
+			}
+
+			ll.add(list);
+		}
+		return ll;
+	}
+}
