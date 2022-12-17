97d9cd865988033a8f13c381c35c7f6327512545
Stephen <55069019+S709394@users.noreply.github.com>
2022-12-17 00:09:19 -0500

    ArrayList +  2 for loop

--- Leetcode_119/src/com/leetcode/Leetcode_119_try1.java
+++ Leetcode_119/src/com/leetcode/Leetcode_119_try1.java
@@ -1,5 +1,19 @@
 package com.leetcode;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Leetcode_119_try1 {
-
+	public static void main(String[] args) {
+		System.out.println(getRow (5));
+	}
+	public  static List<Integer> getRow(int idx) {
+		List<Integer> res = new ArrayList<>();
+		for (int i = 0; i <= idx; i++) {
+			res.add(1);
+			for (int j =i-1; j>0  ; j--) {
+				System.out.println("j :" + j);			
+				res.set(j,res.get(j-1)+res.get(j));
+			}
+		}
+		return res;
+	}
 }
