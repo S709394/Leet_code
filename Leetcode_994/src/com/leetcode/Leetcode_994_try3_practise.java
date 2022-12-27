70427038f59d3abbd65420395a451914c5924258
Stephen <55069019+S709394@users.noreply.github.com>
2022-12-26 21:32:54 -0500

    Try3 , practise
    //	Runtime
    //	1 ms
    //	Beats
    //	100%
    //	Memory
    //	41.4 MB
    //	Beats
    //	99.63%

--- Leetcode_994/src/com/leetcode/Leetcode_994_try3_practise.java
+++ Leetcode_994/src/com/leetcode/Leetcode_994_try3_practise.java
@@ -3,79 +3,71 @@ package com.leetcode;
 import java.util.LinkedList;
 import java.util.Queue;
 
-
-//Runtime
-//1 ms
-//Beats
-//100%
-//Memory
-//42.2 MB
-//Beats
-//70.55%
-public class Leetcode_994_try2 {
-	public static void main(String[] args) {
-
-	}
-
+//practise  , same as try 1 
+public class Leetcode_994_try3_practise {
+	
+//	Runtime
+//	1 ms
+//	Beats
+//	100%
+//	Memory
+//	41.4 MB
+//	Beats
+//	99.63%
 	public int orangesRotting(int[][] grid) {
 		int M = grid.length;
 		int N = grid[0].length;
+		Queue<int[]> queue = new LinkedList<>();
 
 		int count = 0;
 
-		Queue<int[]> queue = new LinkedList<>();
-
-		for (int r = 0; r < M; r++) {
-			for (int c = 0; c < N; c++) {
-				if (grid[r][c] == 1) {
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				if (grid[i][j] == 1) {
 					count++;
 				}
-				if (grid[r][c] == 2) {
-
-					queue.add(new int[] { r, c });
+				if (grid[i][j] == 2) {
+					queue.add(new int[] { i, j });
 				}
-
 			}
 		}
+//		System.out.println( "count" + count ); //test 
 		int round = 0;
 		while (count > 0 && !queue.isEmpty()) {
 			round++;
-
 			int n = queue.size();
 			for (int i = 0; i < n; i++) {
+
 				int[] orange = queue.poll();
 				int r = orange[0], c = orange[1];
 
-				if (r > 0 && grid[r - 1][c] == 1) {
+				if (c + 1 < N && grid[r][c + 1] == 1) {
+					grid[r][c + 1] = 2;
+					queue.add(new int[] { r, c + 1 });
+					count--;
+				}
+				if (c - 1 >= 0 && grid[r][c - 1] == 1) {
+					grid[r][c - 1] = 2;
+					queue.add(new int[] { r, c - 1 });
+					count--;
+				}
+				if (r + 1 < M && grid[r + 1][c] == 1) {
+					grid[r + 1][c] = 2;
+					queue.add(new int[] { r + 1, c });
+					count--;
+				}
+
+				if (r - 1 >= 0 && grid[r - 1][c] == 1) {
 					grid[r - 1][c] = 2;
 					queue.add(new int[] { r - 1, c });
 					count--;
 				}
 
-				if (c > 0 && grid[r][c - 1] == 1) {
-					grid[r][c - 1] = 2;
-					queue.add(new int[] { r, c - 1 });
-					count--;
-				}
-				if (r < M - 1 && grid[r + 1][c] == 1) {
-					grid[r + 1][c] = 2;
-					queue.add(new int[] { r + 1, c });
-					count--;
-				}
-				if (c < N - 1 && grid[r][c + 1] == 1) {
-					grid[r][c + 1] = 2;
-					queue.add(new int[] { r, c + 1 });
-					count--;
-				}
-
 			}
-
 		}
-
 		if (count == 0) {
 			return round;
 		}
 		return -1;
-
 	}
 }
