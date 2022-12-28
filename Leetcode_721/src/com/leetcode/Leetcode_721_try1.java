7f4050033c45d4b60b2549fa2184f89d7f3ba293
Stephen <55069019+S709394@users.noreply.github.com>
2022-12-27 22:52:45 -0500

    First try

---
+++ Leetcode_721/src/com/leetcode/Leetcode_721_try1.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Leetcode_721_try1 {
+	public static void main(String[] args) {
+
+	}
+
+	public List<List<String>> accountsMerge(List<List<String>> accounts) {
+		Map<String, Set<String>> graph = new HashMap<>(); // <email node ,neibo
+		Map<String,String > name = new HashMap <> () ;
+		
+		for (List <String> account : accounts) {
+			
+		}
+	}
+}
