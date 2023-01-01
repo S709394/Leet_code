6ae0e912acec9965154c57dc3d11f0110261c5f1
Stephen <55069019+S709394@users.noreply.github.com>
2022-12-31 23:54:13 -0500

    Try4 using Queue but not  recursive

--- Leetcode_102/src/com/leetcode/Leetcode_102_try4.java
+++ Leetcode_102/src/com/leetcode/Leetcode_102_try4.java
@@ -8,6 +8,8 @@ import java.util.Queue;
 
 //import Leetcode_103_try1.TreeNode;
 
+//Using queue but not recursive 
+
 public class Leetcode_102_try4 {
 	public class TreeNode {
 		int val;
@@ -35,14 +37,26 @@ public class Leetcode_102_try4 {
 			return res;
 		}
 		Queue<TreeNode> q = new LinkedList<>();
-
+		q.offer(root);
 		while (!q.isEmpty()) {
 			int size = q.size();
+
+			// mark the curr level as LinedList
 			List<Integer> level = new LinkedList<>();
 			for (int i = 0; i < size; i++) {
 				TreeNode curr = q.poll();
+				level.add(curr.val);
+				if (curr.left != null) {
+					q.offer(curr.left);
+				}
+				if (curr.right != null) {
+					q.offer(curr.right);
+				}
+
 			}
+			res.add(level);
 		}
+		return res;
 
 	}
 }
