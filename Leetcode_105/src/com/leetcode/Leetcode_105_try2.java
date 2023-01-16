bf4540f31461803d8f6090b3f9ceadd4efa4ffff
Stephen <55069019+S709394@users.noreply.github.com>
2023-01-15 23:42:17 -0500

    Practise_ going to finish

---
+++ Leetcode_105/src/com/leetcode/Leetcode_105_try2.java
@@ -0,0 +1,65 @@
+package com.leetcode;
+
+import java.util.HashMap;
+
+//import com.leetcode.Leetcode_105_try1.TreeNode;
+
+public class Leetcode_105_try2 {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+
+	}
+
+	HashMap<Integer, Integer> valToIdx = new HashMap<>();
+
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		int pLen = preorder.length;
+		int iLen = inorder.length;
+
+		// *** try pre length
+		for (int i = 0; i < pLen; i++) {
+			valToIdx.put(preorder[i], i);	
+		}
+
+		return build(preorder, 0, pLen, inorder, 0, iLen);
+	}
+
+	public TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+		// root val
+
+		int rootVal = preorder[preStart];
+		// index (of the root)
+
+		int index = valToIdx.get(rootVal);
+
+		// leftSize
+
+		int leftSize = index - inStart;
+		
+		
+		//Creat new root 
+		TreeNode root = new TreeNode (rootVal);
+		
+		root .right =  build
+		
+		
+		
+
+	}
+
+}
