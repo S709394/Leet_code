c9595d25e6f148315b3fa8a17402236dd459a543
Stephen <55069019+S709394@users.noreply.github.com>
2023-01-27 23:52:55 -0500

    1st try  , does not works so far  Leetcode 300
    Should use DP way

---
+++ Leetcode_300/src/com/leetcode/Leetcode_300_try1.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+
+//does  not works at all 
+public class Leetcode_300_try1 {
+//	1 <= nums.length <= 2500
+//			-104 <= nums[i] <= 104
+	
+	public int lengthOfLIS(int[] nums) {
+        int curMin = -10001 ;
+        int cnt = 0 ;
+        
+        int maxCnt =0 ;
+        
+        curMin = nums[0] ;
+        for (int i = 1 ; i < nums.length  ; i ++) {
+        	
+        	if( curMin < nums[i]) {
+        		cnt ++ ;
+        		
+        		curMin = nums[i] ;
+        	}else {
+        		cnt = 0 ;
+        	}
+        	
+        	if (maxCnt < cnt ) {
+        		maxCnt = cnt ;
+        	}
+        	
+        	
+        }
+        
+        
+        return maxCnt ;
+    }
+}
