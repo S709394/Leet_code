7ed5a14e86240e91bbb955fe11229ecb55924e91
Stephen <55069019+S709394@users.noreply.github.com>
2023-02-02 23:01:51 -0500

    Try1 _  just a forloop_sample
    / 1 ms
    		// Beats
    		// 99.98%
    		// Memory
    		// 43.4 MB
    		// Beats
    		// 69.11%

--- Leetcode_412/src/com/leetcode/Leetcode_412_try1.java
+++ Leetcode_412/src/com/leetcode/Leetcode_412_try1.java
@@ -5,22 +5,56 @@ import java.util.List;
 
 public class Leetcode_412_try1 {
 
+	// looks not good : (
 	public static void main(String[] args) {
-		int n = 4;
+		int n = 15;
 
 		System.out.println(fizzBuzz(n));
 
 	}
 
 	public static List<String> fizzBuzz(int n) {
-		List<String> list = new ArrayList<>();
 
-		for (int i = 1; i <= n; i++) {
+		//works 
+		        //Runtime
+		// 1 ms
+		// Beats
+		// 99.98%
+		// Memory
+		// 43.4 MB
+		// Beats
+		// 69.11%
+		        List<String> list = new ArrayList<>();
 
-			String str = Integer.toString(i);
-			list.add(str);
-		}
+				for (int i = 1; i <= n; i++) {
+					// System.out.println(" i : " + i);
+					
+		             if (i % 5 != 0&& i % 3 != 0) {
+						String str = Integer.toString(i);
+						list.add(str);
+					}else {
+		                if (i % 5 == 0 && i % 3 == 0) {
+							list.add("FizzBuzz");
 
-		return list;
+						}else {
+						if (i % 5 == 0) {
+
+							list.add("Buzz");
+						}
+						if (i % 3 == 0) {
+
+							list.add("Fizz");
+
+						}}
+					// }
+		            }
+
+					// if (i % 5 == 0 || i % 3 == 0) {
+						
+		           
+
+				}
+
+				return list;
 	}
 }
