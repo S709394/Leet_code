b1afb7d7eb7550ba3b4d5557d63b14c7862b22b1
Stephen <55069019+S709394@users.noreply.github.com>
2023-03-04 23:17:19 -0500

    Try1_ unfinished_binarySearch

---
+++ Leetcode_1283/src/com/leetcode/Leetcode_1283_try1.java
@@ -0,0 +1,42 @@
+package com.leetcode;
+
+public class Leetcode_1283_try1 {
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 5, 9 };
+		int threshold = 6;
+		System.out.println(smallestDivisor(nums, threshold));
+	}
+
+	public static  int smallestDivisor(int[] nums, int threshold) {
+
+		int right = 1, left = 1;
+
+		int res = Integer.MAX_VALUE;
+
+		for (int i = 0; i < nums.length; i++) {
+			right = Math.max(right, nums[i]);
+		}
+		while (left < right) {
+			int sum = 0;
+			int mid = left + (right - left) / 2;
+
+			for (int i = 0; i < nums.length; i++) {
+				sum += nums[i] / mid;
+				if (sum % mid != 0) {
+					sum++;
+				}
+			}
+			res = Math.min(sum, res);
+
+			if (sum <= threshold) {
+				left = mid;
+			} else {
+				right = mid + 1;
+			}
+
+		}
+
+		return res;
+
+	}
+}
