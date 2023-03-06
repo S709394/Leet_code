c040a341a13babd605862795c56458c892c0e1f1
Stephen <55069019+S709394@users.noreply.github.com>
2023-03-05 21:10:14 -0500

    Try2 _from others_

--- Leetcode_1283/src/com/leetcode/Leetcode_1283_try1.java
+++ Leetcode_1283/src/com/leetcode/Leetcode_1283_try1.java
@@ -7,7 +7,7 @@ public class Leetcode_1283_try1 {
 		System.out.println(smallestDivisor(nums, threshold));
 	}
 
-	public static  int smallestDivisor(int[] nums, int threshold) {
+	public static int smallestDivisor(int[] nums, int threshold) {
 
 		int right = 1, left = 1;
 
@@ -16,24 +16,23 @@ public class Leetcode_1283_try1 {
 		for (int i = 0; i < nums.length; i++) {
 			right = Math.max(right, nums[i]);
 		}
-		while (left < right) {
-			int sum = 0;
-			int mid = left + (right - left) / 2;
+		int sum = 0;
+		int mid = left + (right - left) / 2;
 
-			for (int i = 0; i < nums.length; i++) {
-				sum += nums[i] / mid;
-				if (sum % mid != 0) {
-					sum++;
-				}
-			}
-			res = Math.min(sum, res);
-
-			if (sum <= threshold) {
-				left = mid;
-			} else {
-				right = mid + 1;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i] / mid;
+			if (sum % mid != 0) {
+				sum++;
 			}
 
+			System.out.println("sum :" + sum); // test
+		}
+		res = Math.min(sum, res);
+
+		if (sum <= threshold) {
+			left = mid;
+		} else {
+			right = mid + 1;
 		}
 
 		return res;
---
+++ Leetcode_1283/src/com/leetcode/Leetcode_1283_try2_fromOther.java
@@ -0,0 +1,42 @@
+package com.leetcode;
+
+public class Leetcode_1283_try2_fromOther {
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 5, 9 };
+		int threshold = 6;
+		System.out.println(smallestDivisor(nums, threshold));
+	}
+
+	public static int smallestDivisor(int[] nums, int threshold) {
+		int left = 1;
+        int right = 0;
+        for(int num:nums){
+            right = Math.max(num,right);
+        }
+
+        while(left<right){
+            int mid = (right-left)/2+left;
+            int sum = 0;
+            for(int num:nums){
+                sum += (num+mid-1)/mid;
+                
+                System.out.println("sum : " + sum );
+            }
+            System.out.println("mid : " + mid );
+            if(sum<=threshold){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+            System.out.println("right : " + right );
+            System.out.println("left : " + left );
+        }
+
+        return right;
+//
+//yu-niang-niang
+//https://leetcode.cn/problems/find-the-smallest-divisor-given-a-threshold/solution/-by-yu-niang-niang-63pj/
+//LeetCode
+//
+	}
+}
