bd1731cf7e3961c173cc96ac5a1ebf63713c2025
Stephen <55069019+S709394@users.noreply.github.com>
2023-03-18 15:49:14 -0400

    Practise_submit_again

--- Leetcode_106/src/com/leetcode/Leetcode_106_try9_practise.java
+++ Leetcode_106/src/com/leetcode/Leetcode_106_try9_practise.java
@@ -24,18 +24,33 @@ public class Leetcode_106_try9_practise {
 		}
 	}
 
-	public TreeNode buildTree(int[] inOrder, int[] postOrder) {
-		HashMap<Integer, Integer> hm = new HashMap<>();
+	HashMap<Integer, Integer> hm = new HashMap<>();
 
+	public TreeNode buildTree(int[] inOrder, int[] postOrder) {
+
+		int inLength = inOrder.length, postLength = postOrder.length;
 		for (int i = 0; i < inOrder.length; i++) {
 			hm.put(inOrder[i], i);
 		}
 
-		int rootVal = postorder[postorder.length - 1];
-		//index of the 
-		int index = hm.get( rootVal );
-		
-		int leftRootSize =  index;
+		return buildTree(inOrder, 0, inLength - 1, postOrder, 0, postLength - 1);
+	}
 
+	public TreeNode buildTree(int[] inOrder, int inStart, int inEnd, int[] postOrder, int postStart, int postEnd) {
+
+		if (inStart > inEnd) {
+			return null;
+		}
+
+//		int rootVal = postOrder[postOrder.length - 1];//wrong  
+		int rootVal = postOrder[postEnd];// wrong
+		// index of the
+		int index = hm.get(rootVal);
+
+		int leftRootSize = index - inStart;
+		TreeNode node = new TreeNode(rootVal);
+		node.left = buildTree(inOrder, inStart, index - 1, postOrder, postStart, postStart + leftRootSize - 1);
+		node.right = buildTree(inOrder, index + 1, inEnd, postOrder, postStart + leftRootSize, postEnd - 1);
+		return node;
 	}
 }
