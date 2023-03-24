2ee03700017f8477756a9c0e35716313fc894e5b
Stephen <55069019+S709394@users.noreply.github.com>
2023-03-23 21:37:25 -0400

    An easy question
    String .valueOf( cArr)

---
+++ Leetcode_1528/src/com/Leetcode_1528_try1.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Leetcode_1528_try1 {
+	public static void main(String[] args) {
+		
+	}
+
+	public String restoreString(String s, int[] indices) {
+		//easy
+		char [] cArr = new char [indices.length];
+		
+		for (int i = 0 ; i < indices.length ; i ++) {
+			
+			cArr[indices [i]] = s.charAt(i);
+		}
+		return String.valueOf(cArr);
+	}
+}
