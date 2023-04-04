32adf26a907da2f95591a12d88e460f62afb5e6c
Stephen <55069019+S709394@users.noreply.github.com>
2023-04-03 22:31:45 -0400

    Add files via upload

--- Leetcode_2529/src/com/leetcode/Leetcode_2529_try1.java
+++ Leetcode_2529/src/com/leetcode/Leetcode_2529_try1.java
@@ -2,18 +2,38 @@ package com.leetcode;
 
 public class Leetcode_2529_try1 {
 	public static void main(String[] args) {
-		int[] nums = { -2, -1, -1, 1, 2, 3 };
+//		int[] nums = { -2, -1, -1, 1, 2, 3 };
 
-		System.out.println();
+		int[] nums = { -2, -1, -1, 0, 0, 0, 1, 2, 3 };
+
+		System.out.println(lessThanZeroIdx(nums));
 
 	}
 
-	public int maximumCount(int[] nums) {
-
+//	public static int maximumCount(int[] nums) {
+//
+//		
+//	}
+	
+	public static int lessThanZeroIdx (int [] nums) {
 		int nLen = nums.length;
 		int left = 0, right = nLen - 1;
-		
-		
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			System.out.println(mid);
+			System.out.println("left"  +left );
+			System.out.println("r"  +right );
+
+			if (nums[mid] > 0) {
+				right = mid;
+			} else {
+				left = mid - 1;
+			}
+
+		}
+
+		return left;
 
 	}
 }
