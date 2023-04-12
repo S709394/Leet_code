843210129f98af5c0e84f98eb3d56f30abd3e15d
Stephen <55069019+S709394@users.noreply.github.com>
2023-04-12 01:13:14 -0400

    Add files via upload

---
+++ Leetcode_2529/src/com/leetcode/Leetcode_2529_try6_binarySearch.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+public class Leetcode_2529_try6_binarySearch {
+public static void main(String[] args) {
+		
+		
+//		int[] nums = { -2, -1, -1, 1, 2, 3 };
+
+		int[] nums = { -2, -1, -1, 0, 0, 0, 1, 2, 3 };
+
+		System.out.println(( maximumCount  (nums));
+		// https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/solutions/3017003/c-java-python3-binary-search/?orderBy=most_votes
+		
+		public  static  int maximumCount(int[] nums) { 
+			
+		}
+		public  int getIdx(int[] nums, int target) { 
+			int left = 0 ; int right =  nums .length;
+			while (left < right ) {
+				
+				int mid = left + (right -left )/2;
+				
+				if (nums [ mid ] > target) {
+					
+				}
+			}
+		}
+}
