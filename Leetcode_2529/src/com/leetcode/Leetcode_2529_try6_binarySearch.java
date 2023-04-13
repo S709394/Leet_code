7e9f722321ee35541777c5fbdcf39afcf349eeca
Stephen <55069019+S709394@users.noreply.github.com>
2023-04-13 10:20:40 -0400

    Try6_works well

--- Leetcode_2529/src/com/leetcode/Leetcode_2529_try6_binarySearch.java
+++ Leetcode_2529/src/com/leetcode/Leetcode_2529_try6_binarySearch.java
@@ -1,21 +1,21 @@
 package com.leetcode;
 
 public class Leetcode_2529_try6_binarySearch {
-public static void main(String[] args) {
-		
-		
+	public static void main(String[] args) {
+
 //		int[] nums = { -2, -1, -1, 1, 2, 3 };
 
 		int[] nums = { -2, -1, -1, 0, 0, 0, 1, 2, 3 };
 
-		System.out.println(( maximumCount  (nums));
+		System.out.println(maximumCount(nums));
+//
 		// https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/solutions/3017003/c-java-python3-binary-search/?orderBy=most_votes
-}
+	}
 
 	public static int maximumCount(int[] nums) {
 		int negtive = getIdx(nums, 0);
 		int moreThanZero = nums.length - getIdx(nums, 1);
-
+		return Math.max(moreThanZero, negtive);
 	}
 
 	public static int getIdx(int[] nums, int target) {
@@ -31,4 +31,6 @@ public static void main(String[] args) {
 				right = mid;
 			}
 		}
+		return left;
 	}
+}
