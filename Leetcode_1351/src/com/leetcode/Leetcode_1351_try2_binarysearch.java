1a2a3a98f56fc47b2a0ab9eabecc42102b595ebb
Stephen <55069019+S709394@users.noreply.github.com>
2023-04-20 17:41:57 -0400

    Does not works , need to fix

--- Leetcode_1351/src/com/leetcode/Leetcode_1351_try2_binarysearch.java
+++ Leetcode_1351/src/com/leetcode/Leetcode_1351_try2_binarysearch.java
@@ -4,7 +4,7 @@ public class Leetcode_1351_try2_binarysearch {
 	public static void main(String[] args) {
 		//  TRY THE 2ND CASE
 		// going to try 2
-		int[][] grid = { { 4, 3, 2, -1 }, { 3, 2, 1, -1 }, { 1, 1, -1, -2 }, { -1, -1, -2, -3 } };
+//		int[][] grid = { { 4, 3, 2, -1 }, { 3, 2, 1, -1 }, { 1, 1, -1, -2 }, { -1, -1, -2, -3 } };
 //		int[][] grid = { { 3, 2 }, { 1, 0 } };
 		int[][] grid = { { 5,1,0 }, {-5,-5,-5 } };
 		System.out.println(countNegatives(grid));
@@ -13,6 +13,7 @@ public class Leetcode_1351_try2_binarysearch {
 	public static int countNegatives(int[][] grid) {
 		int m = grid.length;
 		int n = grid[0].length;
+		System.out.println( "m: " + m + "n : " + n );
 		// i , j
 
 		int i = 0;
@@ -22,17 +23,17 @@ public class Leetcode_1351_try2_binarysearch {
 
 		while (j >= 0 && i < m) {
 
-//			System.out.println("i :" + i + "  /n j : " + j);
+			System.out.println("i :" + i + "  /n j : " + j);
 			if (grid[i][j] >= 0) {
 				res += n - j-1 ;
-//				System.out.println("res : " + res);
+				System.out.println("res : " + res);
 				i++;
 //				System.out.println(" j-- ");
 			} else {
 				j--;
 			}
 		}
-		if (grid[n - 1][0] < 0) {
+		if (grid[m-1][0] < 0) {
 			res += n;
 		}
 
