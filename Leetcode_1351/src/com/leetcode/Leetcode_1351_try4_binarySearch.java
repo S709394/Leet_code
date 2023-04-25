0188d19905f27872b659ae92508390124f3ee806
Stephen <55069019+S709394@users.noreply.github.com>
2023-04-24 23:30:24 -0400

    Add files via upload

--- Leetcode_1351/src/com/leetcode/Leetcode_1351_try4_binarySearch.java
+++ Leetcode_1351/src/com/leetcode/Leetcode_1351_try4_binarySearch.java
@@ -2,9 +2,7 @@ package com.leetcode;
 
 public class Leetcode_1351_try4_binarySearch {
 	public static void main(String[] args) {
-		// DOes not works
-		// TRY THE 2ND CASE
-		// going to try 2
+		
 //		int[][] grid = { { 4, 3, 2, -1 }, { 3, 2, 1, -1 }, { 1, 1, -1, -2 }, { -1, -1, -2, -3 } };
 //		int[][] grid = { { 3, 2 }, { 1, 0 } };
 //		int[][] grid = { { 5, 1, 0 }, { -5, -5, -5 } };
@@ -17,16 +15,24 @@ public class Leetcode_1351_try4_binarySearch {
 		
 //		https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/solutions/510249/java-python-3-2-similar-o-m-n-codes-w-brief-explanation-and-analysis/?orderBy=most_votes
 		
+		//edge 
 		int m = grid.length;
 		int n = grid[0].length;
 
+		//row : start   from top 
 		int r = 0;
+		
+		// Column front right to left 
 		int c = n - 1;
+		
 		int cnt = 0;
 
 		while (c >= 0 && r < m) {
+			System.out.println("r :" + r + "   c : " + c);
 			if (grid[r][c] < 0) {
 				c--;
+				
+				// count the  negatives num in this row
 				cnt += m - r;
 			}else {
 				r++;
