def78a2652999cb76b8a1ed1ea85900c15e297c1
Stephen <55069019+S709394@users.noreply.github.com>
2023-05-12 01:38:19 -0400

    Try2 works well

--- Leetcode_2094/src/com/leetcode/Leetcode_2094_try2.java
+++ Leetcode_2094/src/com/leetcode/Leetcode_2094_try2.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode_2094_try2 {
 	public static void main(String[] args) {
 		int nums[] = { 1, 3, 5, 2 };
@@ -7,13 +10,15 @@ public class Leetcode_2094_try2 {
 		System.out.println(findEvenNumbers(nums));
 	}
 
-	public int[] findEvenNumbers(int[] digits) {
+	public static int[] findEvenNumbers(int[] digits) {
 		int[] cnt = new int[10];
 
 		for (int i : digits) {
 			cnt[i]++;
-
+			
 		}
+
+		List<Integer> res = new ArrayList<>();
 
 		for (int i = 100; i < 1000; i += 2) {
 
@@ -22,11 +27,31 @@ public class Leetcode_2094_try2 {
 			String numStr = i + "";
 
 			for (int j = 0; j < 3; j++) {
-				
-				 cur [ j] =numStr.charAt(i ) -'0' ;
+
+				curCnt[numStr.charAt(j) - '0']++;
 			}
 
-		}
+			boolean isVaild = true;
 
+			for (int j = 0; j < curCnt.length; j++) {
+				if (curCnt[j] > cnt[j]) {
+					isVaild = false;
+					break;
+				}
+
+			}
+
+			if (isVaild) {
+				res.add(i);
+			}
+			
+
+		}
+		int [] resArr = new int [res.size()];
+		
+		for (int j =0 ; j < res .size(); j ++  ) {
+			resArr[j] = res.get(j);
+		}
+		return resArr;
 	}
 }
