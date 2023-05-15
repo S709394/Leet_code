85e59f2e886026affa1ac5dbe5fde080868831bf
Stephen <55069019+S709394@users.noreply.github.com>
2023-05-15 10:38:17 -0400

    Add files via upload

--- Leetcode_1295/src/com/leetcode/Leetcode_1295_try2.java
+++ Leetcode_1295/src/com/leetcode/Leetcode_1295_try2.java
@@ -1,5 +1,21 @@
 package com.leetcode;
 
 public class Leetcode_1295_try2 {
+	public static void main(String[] args) {
+		int nums[] = { 555, 901, 482, 1771 };
 
+		System.out.println(findEvenNumbers(nums));
+	}
+
+	public static int findEvenNumbers(int[] nums) {
+
+		int cnt = 0;
+		for (int i : nums) {
+
+			if ((i > 9 && i <= 99) || i > 999 && i <= 9999 ||  i == 100000) {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
 }
