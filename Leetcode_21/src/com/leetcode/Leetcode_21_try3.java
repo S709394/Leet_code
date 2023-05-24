aef6a5383abce19876f5b8610000180d9342fe2b
Stephen <55069019+S709394@users.noreply.github.com>
2023-05-23 23:21:58 -0400

    Does not works so far

--- Leetcode_21/src/com/leetcode/Leetcode_21_try3.java
+++ Leetcode_21/src/com/leetcode/Leetcode_21_try3.java
@@ -21,9 +21,36 @@ public class Leetcode_21_try3 {
 			this.next = next;
 		}
 		public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+			//try3 practise
 			ListNode  dummy = new ListNode (-1 ) ;
 			
+			ListNode p = dummy ;
 			
+			ListNode  p1 = list1 , p2 = list2 ;
+			
+			
+			while (p1!= null && p2!= null ) {
+				if (p1.val<=p2.val ) {
+					p.next=p1;
+					p1=p1.next;
+				}else {
+					
+					p.next=p2;
+					p2=p2.next;
+				}
+				p=p.next;
+				
+			}
+			//  one of the p1 /p2 finished 
+			
+			if( p1 !=null ) {
+				p.next=p1;
+			}
+			
+			if(p2 !=null) {
+				p.next=p2;
+			}
+			return dummy.next;
 		}
 	}	
 	
---
+++ Leetcode_21/src/com/leetcode/Leetcode_21_try4.java
@@ -0,0 +1,5 @@
+package com.leetcode;
+
+public class Leetcode_21_try4 {
+
+}
