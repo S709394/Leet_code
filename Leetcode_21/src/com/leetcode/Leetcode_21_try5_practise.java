c66fa0603fec6c00dc56134445dd96ea7cabbaff
Stephen <55069019+S709394@users.noreply.github.com>
2023-05-24 21:23:58 -0400

    Try5_worksWell

--- Leetcode_21/src/com/leetcode/Leetcode_21_try5_practise.java
+++ Leetcode_21/src/com/leetcode/Leetcode_21_try5_practise.java
@@ -1,8 +1,8 @@
 package com.leetcode;
 
-//import com.leetcode.Leetcode_21_try2_.ListNode;
+//import com.leetcode.Leetcode_21_try4.ListNode;
 
-public class Leetcode_21_try4 {
+public class Leetcode_21_try5_practise {
 	public class ListNode {
 		int val;
 		ListNode next;
@@ -21,33 +21,30 @@ public class Leetcode_21_try4 {
 	}
 
 	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-		// try4
+		
+		//try5
+		ListNode dummy = new ListNode(-1), p1 = list1, p2 = list2;
 
-		ListNode dummy = new ListNode(-1);
-		ListNode p1 = list1, p2 = list2;
 		ListNode p = dummy;
 
 		while (p1 != null && p2 != null) {
-			if (p1.val < p2.val) {
+			if (p1.val <= p2.val) {
 				p.next = p1;
 				p1 = p1.next;
 			} else {
 				p.next = p2;
 				p2 = p2.next;
-
 			}
 			p = p.next;
-
 		}
+
+		//
 		if (p1 != null) {
 			p.next = p1;
-			p = p.next;
 		}
 		if (p2 != null) {
 			p.next = p2;
-			p = p.next;
 		}
-
 		return dummy.next;
 	}
 }
