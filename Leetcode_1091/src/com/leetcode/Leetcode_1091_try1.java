31d2fbe35c2409c467971f0309ae4fd37d3f785e
Stephen <55069019+S709394@users.noreply.github.com>
2023-06-02 23:16:47 -0400

    Add files via upload

--- Leetcode_1091/src/com/leetcode/Leetcode_1091_try1.java
+++ Leetcode_1091/src/com/leetcode/Leetcode_1091_try1.java
@@ -7,27 +7,47 @@ public class Leetcode_1091_try1 {
 	public int shortestPathBinaryMatrix(int[][] grid) {
 		int m = grid.length, n = grid[0].length;
 
-		
-		// make sure not walk as loop
-		boolean[][] visited = new boolean[m][n];
-
 		// top left and right bottom
 		if (grid[0][0] == 1 || grid[m - 1][n - 1] == 1) {
 			return -1;
 		}
 
+		Queue<int[]> q = new LinkedList<>();
 		int[][] dirs = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }, { 1, 1 }, { 1, -1 }, { -1, 1 },
 				{ -1, -1 } };
 
-		Queue<int[]> q = new LinkedList<>();
+		
 
+		// make sure not walk as loop
+		boolean[][] visited = new boolean[m][n];
 		// init the Queue q
 		q.offer(new int[] { 0, 0 });
-		
-		while (!  q.isEmpty()) {
-			 int size =  q.size();
-			 for (int  size = 0 ; size<)
-		}
+		 visited[0][0] = true;
 
+		int pathLen = 1;
+
+		while (!q.isEmpty()) {
+			int sz = q.size();
+			for (int size = 0; size < sz; size++) {
+				int[] cur = q.poll();
+
+				int x = cur[0];
+				int x = cur[1];
+				
+				if(x == m-1 &&y==n-1) {
+					return pathLen;
+				}
+				for (int i = 0 ; i < 8 ; i ++) {
+					if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n && grid[nextX][nextY] == 0
+							&& visited[nextX][nextY] != true) {
+
+					}
+				}
+				
+
+			}
+			pathLen++;
+		}
+		return pathLen;
 	}
 }
