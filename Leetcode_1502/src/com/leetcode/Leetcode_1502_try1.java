2c2aa19e8d81c7cad33ef1da0f1ca72b9f620ab6
Stephen <55069019+S709394@users.noreply.github.com>
2023-06-06 23:51:11 -0400

    Try1

---
+++ Leetcode_1502/src/com/leetcode/Leetcode_1502_try1.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class Leetcode_1502_try1 {
+	public static void main(String[] args) {
+		int[] arr = { 3, 5, 1 };
+		
+	}
+
+	public static boolean canMakeArithmeticProgression(int[] arr) {
+		Arrays.sort(arr);
+		
+		for (int i = 0 ; i < arr.length-1) {
+			
+		}
+	}
+}
