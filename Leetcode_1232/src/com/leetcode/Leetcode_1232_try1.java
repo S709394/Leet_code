cc734a5373121e476842f0326c5d45c303b99503
Stephen <55069019+S709394@users.noreply.github.com>
2023-06-10 23:50:01 -0400

    Can not divide by zero //does not works

--- Leetcode_1232/src/com/leetcode/Leetcode_1232_try1.java
+++ Leetcode_1232/src/com/leetcode/Leetcode_1232_try1.java
@@ -3,7 +3,8 @@ package com.leetcode;
 public class Leetcode_1232_try1 {
 	public static void main(String[] args) {
 //		int[][] coord = new int[][] { { -1, 0 }, { 1, 2 }, { 2, 3 }, { 3, 4 } };
-		int[][] coord = new int[][] {  { 1, 2 }, { 2, 3 }, { 3, 4 } };
+//		int[][] coord = new int[][] { { 1, 2 }, { 2, 3 }, { 3, 4 } };
+		int[][] coord = new int[][] { { 0, 2 }, { 2, 4}, { 4, 6} };
 		System.out.println(checkStraightLine(coord));
 	}
 
@@ -15,7 +16,7 @@ public class Leetcode_1232_try1 {
 		}
 
 		int div = coordinates[1][1] - coordinates[0][1];
-		System.out.println( "div :" + div );//test
+		System.out.println("div :" + div);// test
 		if (div == 0) {
 			for (int i = 2; i < coorLen; i++) {
 				if (coordinates[i][1] != coordinates[0][1]) {
@@ -24,20 +25,24 @@ public class Leetcode_1232_try1 {
 			}
 		} else {
 
-			double ans = coordinates[1][0] - coordinates[0][0] / coordinates[1][1] - coordinates[0][1];
-			System.out.println("ans" + ans);
-			for (int i = 1; i < coorLen -1; i++) {
+			double ans = ((double)coordinates[1][0] - (double)coordinates[0][0] )/
+					((double)coordinates[1][1] -(double) coordinates[0][1]);
+			
+//			System.out.println((double)coordinates[1][1] -(double) coordinates[0][1]);
+			System.out.println("ans: "  + ans);
+			for (int i = 1; i < coorLen - 1; i++) {
 				System.out.println(i); // test
 				int xA = coordinates[i][0];
 				int yA = coordinates[i][1];
 				int xB = coordinates[i + 1][0];
 				int yB = coordinates[i + 1][1];
-				
-				
-				if ((yB-xB) / ( yA-xA ) != ans) {
+
+				double rate = (yB - xB )/ (yA - xA); //test
+				System.out.println("rate" + rate);
+				if (rate != ans) {
 					return false;
 				}
-				
+
 			}
 		}
 		return true;
