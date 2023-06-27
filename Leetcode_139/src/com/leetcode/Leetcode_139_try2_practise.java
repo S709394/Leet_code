84b59ca4fee638d6e10742dd076c4606ccd41e3b
Stephen <55069019+S709394@users.noreply.github.com>
2023-06-26 23:14:36 -0400

    Try2_practise

--- Leetcode_139/src/com/leetcode/Leetcode_139_try2_practise.java
+++ Leetcode_139/src/com/leetcode/Leetcode_139_try2_practise.java
@@ -8,10 +8,10 @@ import java.util.Set;
 public class Leetcode_139_try2_practise {
 
 	public boolean wordBreak(String s, List<String> wordDict) {
-
 		int wordLen = wordDict.size();
+		int strLen = s.length();
 		Set<String> set = new HashSet<>();
-//		boolean  
+		boolean[] boolArr = new boolean[strLen+1];
 
 		int minLen = Integer.MAX_VALUE;
 		for (String str : wordDict) {
@@ -20,9 +20,23 @@ public class Leetcode_139_try2_practise {
 		}
 
 		for (int i = 1; i <= wordLen; i++) {
-			
-			for (int j = i +1)
+			for (int j = i + 1; j > 0; j--) {
 
+				if (i - j + 1 < wordLen) {
+					continue;
+				}
+//				System.out.println(s.substring(j - 1, i)); //test 
+				System.out.println("j-1 :" + (j - 1));//test 
+				System.out.println("i : " + i);//test 
+				System.out.println(s.substring(j - 1, i)); //test
+				boolArr[i] = boolArr[i - 1] && set.contains(s.substring(j - 1, i));
+
+			}
+			if (boolArr[i]) {
+				// System.out.println("i:" + i);
+				break;
+			}
 		}
+		return boolArr[strLen ] ;
 	}
 }
