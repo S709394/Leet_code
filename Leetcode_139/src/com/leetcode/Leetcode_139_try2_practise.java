9124023d8657ba28879af3c6477c017707ef151b
Stephen <55069019+S709394@users.noreply.github.com>
2023-06-27 23:11:43 -0400

    Try3_practise

--- Leetcode_139/src/com/leetcode/Leetcode_139_try2_practise.java
+++ Leetcode_139/src/com/leetcode/Leetcode_139_try2_practise.java
@@ -6,36 +6,45 @@ import java.util.Set;
 //import java.util.Stack;
 
 public class Leetcode_139_try2_practise {
+	public static void main(String[] args) {
+		String str = "leetcode" ;
+		
+	}
 
 	public boolean wordBreak(String s, List<String> wordDict) {
-		int wordLen = wordDict.size();
-		int strLen = s.length();
+		
+		//try2
 		Set<String> set = new HashSet<>();
+//		int wordLen = wordDict.size();
+		int strLen = s.length();
+		
 		boolean[] boolArr = new boolean[strLen+1];
-
+		boolArr[0] = true;
+		
+		
 		int minLen = Integer.MAX_VALUE;
 		for (String str : wordDict) {
 			set.add(str);
 			minLen = Math.min(minLen, str.length());
 		}
 
-		for (int i = 1; i <= wordLen; i++) {
+		for (int i = 1; i <= strLen; i++) {
 			for (int j = i + 1; j > 0; j--) {
 
-				if (i - j + 1 < wordLen) {
+				if (j-1+i < minLen) {
 					continue;
 				}
 //				System.out.println(s.substring(j - 1, i)); //test 
 				System.out.println("j-1 :" + (j - 1));//test 
 				System.out.println("i : " + i);//test 
 				System.out.println(s.substring(j - 1, i)); //test
-				boolArr[i] = boolArr[i - 1] && set.contains(s.substring(j - 1, i));
-
+				boolArr[i] = boolArr[j - 1] && set.contains(s.substring(j - 1, i));
+				if (boolArr[i]) {
+					// System.out.println("i:" + i);
+					break;
+				}
 			}
-			if (boolArr[i]) {
-				// System.out.println("i:" + i);
-				break;
-			}
+			
 		}
 		return boolArr[strLen ] ;
 	}
---
+++ Leetcode_139/src/com/leetcode/Leetcode_139_try3_practise.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Leetcode_139_try3_practise {
+	public boolean wordBreak(String s, List<String> wordDict) {
+		Set<String> set = new HashSet<>();
+		
+		int sLen = s.length();
+		
+		boolean [] boolArr =new boolean [sLen +1] ;
+		
+		boolArr [0] = true ;
+		
+		int wordMinLen = Integer.MAX_VALUE ;
+		
+		for (int i = 1 ; i <= strLen ; i ++) {
+			
+			for ( j=i+1 ; j < )
+			
+		}
+
+	}
+}
