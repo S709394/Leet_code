25d4122c7145f942014e37f34bffd69833b8b289
Stephen <55069019+S709394@users.noreply.github.com>
2023-07-05 00:57:28 -0400

    859_try1

--- Leetcode_859/src/com/leetcode/Leetcode_859_try1.java
+++ Leetcode_859/src/com/leetcode/Leetcode_859_try1.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class Leetcode_859_try1 {
 	public static void main(String[] args) {
-		String s = "bac";
+//		String s = "bac";
+		String s = "aaa";
 
 		String goal = "abc";
 		System.out.println(buddyStrings(s, goal));
@@ -17,30 +18,35 @@ public class Leetcode_859_try1 {
 		if (sLen != goalLen)
 			return false;
 		char[][] diffs = new char[3][2];
-		for (int l = 0; l < 2; l++) {
+		
+//		Arrays.fill(diffs[1], 'A');
+		
+//		System.out.println("aaa: "  + diffs [0] [0]); //test 
+		for (int l = 0; l < 3; l++) {
 			Arrays.fill(diffs[l], 'A');
-			
+//			System.out.println("bbb: "  + diffs [l] [1]); //test 
 			
 		}
-		System.out.println("aaa: "  + diffs [0] [0]); //test 
+		
+//		System.out.println("aaa: "  + diffs [0] [0]); //test 
 		for (int i = 0; i < sLen; i++) {
 
 			// all lower case
-			System.out.println( "i:" +i);
+//			System.out.println( "i:" +i);
 			int idx = 0;
 			if (s.charAt(i) != goal.charAt(i)) {
 				diffs[i][0] = s.charAt(i);
 
-				System.out.println(s.charAt(i));//test
+//				System.out.println(s.charAt(i));//test
 				diffs[i][1] = goal.charAt(i);
-				System.out.println(goal.charAt(i)); //test 
+//				System.out.println(goal.charAt(i)); //test 
 				idx++;
 				
 			}
 			if (idx > 2) {
 				break;
 			}
-			System.out.println("diffs :"  +  diffs [2][0]);//test
+//			System.out.println("diffs :"  +  diffs [2][0]);//test
 /// 
 //			for (int j = 0; j < 2; j++) {
 //				for (int k = 0; k < 3; k++) {
@@ -50,7 +56,11 @@ public class Leetcode_859_try1 {
 
 		}
 
-		if (diffs[2][0] != 'A' || diffs[2][1] != 'A') {
+		if (diffs[0][0] == 'A'||diffs[2][0] != 'A' || diffs[2][1] != 'A') {
+			
+			if(diffs[0][0]  == diffs[0][1]  && diffs[1][0]== diffs[1][1]  ) {
+				return true ;
+			}
 			return false;
 		}
 
