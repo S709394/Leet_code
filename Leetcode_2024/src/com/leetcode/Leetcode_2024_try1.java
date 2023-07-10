13eb6c7c3d6f28dd4a2492c2993e333a9d58d70c
Stephen <55069019+S709394@users.noreply.github.com>
2023-07-09 22:25:08 -0400

    2024_no Idea so far

--- Leetcode_2024/src/com/leetcode/Leetcode_2024_try1.java
+++ Leetcode_2024/src/com/leetcode/Leetcode_2024_try1.java
@@ -8,27 +8,25 @@ public class Leetcode_2024_try1 {
 
 	}
 
-	public static  int maxConsecutiveAnswers(String answerKey, int k) {
-		
-		//no idea so far
-		 
+	public static int maxConsecutiveAnswers(String answerKey, int k) {
+
+		// no idea so far
+
 //		int []  cntArr = new  int [] ;
-		
-		char [] chars  =answerKey  .toCharArray();
-		
-		
-		int res = 0 , max = 0  ,  count  [] = new int [128] ;
-		
-		
-		for ( int i = 0 ; i < chars.length ; i ++) {
-			if( res - (max =Math.max(max,  ++ count [chars [ i]])) < k ) {
-				
-			}else {
-				count [chars[i-res]]-- ;
+
+		char[] chars = answerKey.toCharArray();
+
+		int res = 0, max = 0, count[] = new int[128];
+
+		for (int i = 0; i < chars.length; i++) {
+			if (res - (max = Math.max(max, ++count[chars[i]])) < k) {
+
+			} else {
+				count[chars[i - res]]--;
 			}
 		}
-		
-		
+
+		return res;
 	}
 
 }
