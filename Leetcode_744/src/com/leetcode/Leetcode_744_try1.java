88e9f8e1f5bc7af2c5acc96f8cbc0cce9372ce17
Stephen <55069019+S709394@users.noreply.github.com>
2023-07-23 15:07:55 -0400

    Normal one _  not the binary search one

---
+++ Leetcode_744/src/com/leetcode/Leetcode_744_try1.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+public class Leetcode_744_try1 {
+	public static void main(String[] args) {
+
+	}
+
+	public static char nextGreatestLetter(char[] letters, char target) {
+		int len = letters.length;
+//		int res = -1 ;
+		for ( int i = 0 ; i < len ; i ++) {
+			if( (letters[i]-target) >0 ) {
+				return letters [i] ;
+			}
+		}
+		return letters[0];
+	}
+}
