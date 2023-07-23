5bfb8c31bf7e1eba76fad41b5cfd555096211fc4
Stephen <55069019+S709394@users.noreply.github.com>
2023-07-22 22:36:32 -0400

    Add files via upload

---
+++ Leetcode_1004/src/com/leetcode/Leetcode_1004_try2.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+public class Leetcode_1004_try2 {
+
+	// try2
+
+	public int longestOnes(int[] nums, int k) {
+		int zeroCnt = 0, res = 0, start = 0;
+
+		for (int end = 0; end < nums.length; end++) {
+			if (nums[end] == 0) {
+				zeroCnt++;
+			}
+
+			while (zeroCnt > k) {
+				if (nums[start] == 0) {
+					zeroCnt--;
+				}
+
+				start++;
+
+			}
+
+			res = Math.max(res, end - start + 1);
+		}
+		return res;
+	}
+
+}
