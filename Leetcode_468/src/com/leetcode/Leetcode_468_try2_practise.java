e65d3c770b5e8baf3a34db987451275d64b6566d
Stephen <55069019+S709394@users.noreply.github.com>
2023-08-01 21:25:21 -0400

    Try3 _468

--- Leetcode_468/src/com/leetcode/Leetcode_468_try2_practise.java
+++ Leetcode_468/src/com/leetcode/Leetcode_468_try2_practise.java
@@ -1,10 +1,10 @@
 package com.leetcode;
 
-public class Leetcode_468_try1 {
+public class Leetcode_468_try2_practise {
 	public static void main(String[] args) {
-		
+
 //		/try1  works well 
-		
+
 //		String IP = "172.16.254.1";
 
 		// 2001:0db8:85a3:0:0:8A2E:0370:7334
@@ -13,14 +13,11 @@ public class Leetcode_468_try1 {
 	}
 
 	public static String validIPAddress(String IP) {
-		
-		//try1  //works well
-		
 		if (isIPv4(IP)) {
 			return "IPv4";
+
 		} else if (isIPv6(IP)) {
 			return "IPv6";
-
 		} else {
 			return "Neither";
 		}
@@ -28,32 +25,27 @@ public class Leetcode_468_try1 {
 
 	public static boolean isIPv4(String IP) {
 		String[] arr = IP.split("\\.", -1);
+
 		for (String str : arr) {
-//			System.out.println(str); // test
 			try {
-				
-				//
 				if (Integer.parseInt(str) > 255 || (str.length() > 1 && str.charAt(0) == '0')) {
 					return false;
 				}
+
 			} catch (NumberFormatException e) {
 				return false;
 			}
 		}
 
 		return arr.length == 4;
-
 	}
 
 	public static boolean isIPv6(String IP) {
 		String[] arr = IP.split("\\:", -1);
 		for (String str : arr) {
-//			System.out.println(str); // test
 			try {
-				
-				// 2 ^16  =  65335
 				if (Integer.parseInt(str, 16) > 65335 || str.length() > 4) {
-					return false;
+					return false ;
 				}
 			} catch (NumberFormatException e) {
 				return false;
--- Leetcode_468/src/com/leetcode/Leetcode_468_try3_practise.java
+++ Leetcode_468/src/com/leetcode/Leetcode_468_try3_practise.java
@@ -1,10 +1,10 @@
 package com.leetcode;
 
-public class Leetcode_468_try1 {
+public class Leetcode_468_try3_practise {
 	public static void main(String[] args) {
-		
+		// IPv4 IPv6
 //		/try1  works well 
-		
+
 //		String IP = "172.16.254.1";
 
 		// 2001:0db8:85a3:0:0:8A2E:0370:7334
@@ -12,53 +12,57 @@ public class Leetcode_468_try1 {
 		System.out.println(validIPAddress(IP));
 	}
 
+	// IPv4
+
 	public static String validIPAddress(String IP) {
-		
-		//try1  //works well
-		
 		if (isIPv4(IP)) {
 			return "IPv4";
+
 		} else if (isIPv6(IP)) {
 			return "IPv6";
-
 		} else {
 			return "Neither";
 		}
+
 	}
 
 	public static boolean isIPv4(String IP) {
+
+		// 1 . number < 255 && when number length >1 chatAt (0 ) != '0'
+
 		String[] arr = IP.split("\\.", -1);
+
 		for (String str : arr) {
-//			System.out.println(str); // test
+
 			try {
-				
-				//
 				if (Integer.parseInt(str) > 255 || (str.length() > 1 && str.charAt(0) == '0')) {
 					return false;
 				}
 			} catch (NumberFormatException e) {
 				return false;
 			}
+
 		}
-
 		return arr.length == 4;
-
 	}
 
 	public static boolean isIPv6(String IP) {
 		String[] arr = IP.split("\\:", -1);
-		for (String str : arr) {
-//			System.out.println(str); // test
+		// 1 . number < 65535 && when number length >1 chatAt (0 ) != '0'
+
+		if (arr.length != 8) {
+			return false;
+		}
+
+		for (String str : arr)
 			try {
-				
-				// 2 ^16  =  65335
-				if (Integer.parseInt(str, 16) > 65335 || str.length() > 4) {
+				if (Integer.parseInt(str,16) > 65535 || (str.length() > 4)) {
 					return false;
 				}
+
 			} catch (NumberFormatException e) {
 				return false;
 			}
-		}
-		return arr.length == 8;
+		return true;
 	}
 }
