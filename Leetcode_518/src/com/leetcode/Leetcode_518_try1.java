b7b14efa2e363d90c353972fedcd1c9d899cffc0
Stephen <55069019+S709394@users.noreply.github.com>
2023-08-13 21:45:36 -0400

    Does not works so far

--- Leetcode_518/src/com/leetcode/Leetcode_518_try1.java
+++ Leetcode_518/src/com/leetcode/Leetcode_518_try1.java
@@ -2,6 +2,7 @@ package com.leetcode;
 
 public class Leetcode_518_try1 {
 	public int change(int amount, int[] coins) {
+		//does not works 
 
 		int len = coins.length;
 		int[][] dp = new int[len + 1][amount + 1];
@@ -13,10 +14,16 @@ public class Leetcode_518_try1 {
 
 		for (int i = 1; i <= amount; i++) {
 			for (int j = 1; j <= len; j++) {
-				if(  j - coins[ i] >= 0) {
+				if(  j - coins[ i-1] >= 0) {
+					dp[i] [j] =  dp[i-1][j] +dp[i] [ j -coins[ i-1] ];
 					
+					
+				}else {
+					dp [i ] [j] = dp [i-1] [j];
 				}
 			}
+			
 		}
+		return dp [len] [amount];
 	}
 }
