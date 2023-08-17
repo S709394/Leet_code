40b0dd8aa42a9882a164c5c953dc1dd0389a7f22
Stephen <55069019+S709394@users.noreply.github.com>
2023-08-17 19:25:46 -0400

    Add files via upload

--- Leetcode_239/src/com/leetcode/Leetcode_239_try1.java
+++ Leetcode_239/src/com/leetcode/Leetcode_239_try1.java
@@ -1,6 +1,8 @@
 package com.leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Leetcode_239_try1 {
 	// Monotonic queue
@@ -19,10 +21,41 @@ public class Leetcode_239_try1 {
 
 		}
 		
+		/// max  to get the max elem
 		public int max () {
 			 
 			 return q.getFirst() ;
 		}
+		
+		public void pop (int n) {
+			 if (n== q.getFirst()) {
+				 q.pollFirst() ;
+			 }
+		}
 
 	}
+	 public int[] maxSlidingWindow(int[] nums, int k) {
+		 
+		 MonotonicQueue window  = new MonotonicQueue () ;
+		 List <Integer > res = new ArrayList <> () ; 
+		 
+		 for ( int i = 0  ; i < nums.length ; i ++) {
+			 if(  i < k-1 ) {
+				 window .push(nums [i]);
+			 }else {
+				 window .push(nums [i]);
+				 res .add(window.max()) ;
+				 //remove the old nuw 
+				 window .pop( i  - k +1 );
+			 }
+		 }
+		 
+		 int [] arr = new int [res.size()] ;  
+		 
+		 for (int i=  0 ; i < res.size() ; i ++) {
+			 arr[i ] = res.get(i);
+		 }
+		 
+		 return arr;
+	 }
 }
