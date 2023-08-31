fe3da20d901231dc102138d8ea00101752fa08a8
Stephen <55069019+S709394@users.noreply.github.com>
2023-08-30 23:47:19 -0400

    Add files via upload

---
+++ Leetcode_2483/src/com/leetcode/Leetcode_2483_try1.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+public class Leetcode_2483_try1 {
+		public static void main(String[] args) {
+			
+		}
+		public int bestClosingTime(String customers) {
+			int max_score = 0 ;  int   score = 0 ,  best_hour = -1 ;
+			
+			int len = customers .length() ;
+			
+			for (int i = 0 ; i < len ; i ++) {
+				
+				//when we meet "Y"
+				if ( customers .charAt(i) == "Y") {
+					
+				}
+			}
+		}
+}
