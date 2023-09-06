be0814934aae4ac9b784534eb1a86c1cbc224994
Stephen <55069019+S709394@users.noreply.github.com>
2023-09-06 19:20:09 -0400

    725_try1

---
+++ Leetcode_725/src/com/leetcode/Leetcode_725_try1.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+public class Leetcode_725_try1 {
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode[] splitListToParts(ListNode head, int k) {
+
+	}
+}
