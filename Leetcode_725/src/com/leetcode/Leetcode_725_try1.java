d3864d0b607f342f29f070f26e1c3b0c9850b070
Stephen <55069019+S709394@users.noreply.github.com>
2023-09-07 22:38:13 -0400

    Not test yet

--- Leetcode_725/src/com/leetcode/Leetcode_725_try1.java
+++ Leetcode_725/src/com/leetcode/Leetcode_725_try1.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode_725_try1 {
 	public class ListNode {
 		int val;
@@ -19,6 +22,45 @@ public class Leetcode_725_try1 {
 	}
 
 	public static ListNode[] splitListToParts(ListNode head, int k) {
-
+		   int len = 0 ;
+		   ListNode cur =  head ;
+		   List<ListNode > parts =  new ArrayList <> ()  ;
+		   
+		   while  (cur != null) {
+			   len ++ ;
+			   cur = cur .next;
+		   }
+		   
+		   int base_size = len / k ;  int extra = len % k ;
+		   
+		   for ( int i = 0   ;i  < k ; i ++) {
+			    int part_size = base_size  +  (extra > 0 ?1 : 0 ); 
+			    ListNode part_head = null, part_tail = null;
+			    
+			    System.out.println( "part_size : " + part_size); //test 
+			    for ( int j= 0 ; j < part_size ; j ++) {
+			       	// the 0 
+			    	
+			    	if ( part_head ==null ) {
+			    		part_head = part_tail = cur;
+			    		
+			    	}else {
+			    		part_tail.next = cur ;
+			    		
+			    		part_tail = part_tail .next;
+			    	}
+			    	
+			    	if( cur != null) {
+			    		cur = cur.next;
+			    	}
+			    	
+			    	
+			    }
+			    
+			    /// cut it 
+			    if( part_tail!= null ) {
+			    	part_tail.next = null ;
+			    }
+		   }
 	}
 }
