0f8924a4dab47632d8be08c1a5472d8cd707e9a2
Stephen <55069019+S709394@users.noreply.github.com>
2023-09-10 19:35:41 -0400

    Try2_practise

--- Leetcode_377/src/com/leetcode/Leetcode_377_try1.java
+++ Leetcode_377/src/com/leetcode/Leetcode_377_try1.java
@@ -12,8 +12,16 @@ public class Leetcode_377_try1 {
 	static int []  dp   ;
 	
 	public static  int combinationSum4(int[] nums, int target) {
+		
+		//try1  
+		
+		//https://leetcode.com/problems/combination-sum-iv/solutions/85036/1ms-java-dp-solution-with-detailed-explanation/?envType=daily-question&envId=2023-09-09
+		
+		//store the combination  1 by 1 
 		dp = new int [target +1] ;
 		
+		
+		// default
 		Arrays.fill(dp, -1);
 		
 		dp[0] = 1; 
@@ -23,10 +31,21 @@ public class Leetcode_377_try1 {
 		
 	}
 	
-	public static helper ( int [ ] nums , int target) {
+	public static  int helper ( int [ ] nums , int target) {
 			if ( dp [ target ]!= -1) {
 				return dp[target] ;
 			}
 			
+			int res = 0 ;
+			
+			for ( int i = 0 ; i < nums.length ; i ++) {
+				if( target >= nums [i]) {
+					res +=  helper ( nums , target - nums [i]);  
+				}
+			}
+			
+			dp [target] = res ;
+			
+			return res ;
 	}
 }
