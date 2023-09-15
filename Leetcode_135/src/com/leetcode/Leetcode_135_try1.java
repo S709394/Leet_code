33a47a8a97a612ca0827ec9046a7a853a514e264
Stephen <55069019+S709394@users.noreply.github.com>
2023-09-14 23:28:46 -0400

    Try2

--- Leetcode_135/src/com/leetcode/Leetcode_135_try1.java
+++ Leetcode_135/src/com/leetcode/Leetcode_135_try1.java
@@ -1,5 +1,30 @@
 package com.leetcode;
 
-public class Leetcode_135_try1 {
+import java.util.Arrays;
 
+public class Leetcode_135_try1 {
+	public static void main(String[] args) {
+		
+	}
+	 public int candy(int[] ratings) {
+		 int  n = ratings.length;
+		 int []  candies = new int [n];
+		 Arrays.fill(candies, 1); 
+		 
+		 
+		 //left to right 
+		 for ( int i =1 ; i < n ; i ++) {
+			 if ( ratings [ i ]  >ratings[i-1]) {
+				 candies [i ] = candies [i -1] +1 ;	 
+			 }
+				 
+		 }
+		 for  (int i = n-2 ; i >=0 ; i --) {
+			 if (ratings [i+1] > ratings [i ] ) {
+				 candies [i+1] = Math.max(candies [i] +1, candies[i+1]) ;
+				 
+				 System.out.println(  candies [i+1]  );//
+			 }
+		 }
+	 }
 }
