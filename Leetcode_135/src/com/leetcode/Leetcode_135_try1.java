128230d4a5cd30578dedb89221dd78cdc1490f18
Stephen <55069019+S709394@users.noreply.github.com>
2023-09-15 22:14:33 -0400

    Leetcode_135_try1_finished

--- Leetcode_135/src/com/leetcode/Leetcode_135_try1.java
+++ Leetcode_135/src/com/leetcode/Leetcode_135_try1.java
@@ -4,27 +4,40 @@ import java.util.Arrays;
 
 public class Leetcode_135_try1 {
 	public static void main(String[] args) {
+//		int [] rating  = {1,0,2} ; 
 		
+		int []  rating = {    1 , 0 , 1,2,3,4,5,6} ;
+		System.out.println(  candy (rating));
 	}
-	 public int candy(int[] ratings) {
-		 int  n = ratings.length;
-		 int []  candies = new int [n];
-		 Arrays.fill(candies, 1); 
-		 
-		 
-		 //left to right 
-		 for ( int i =1 ; i < n ; i ++) {
-			 if ( ratings [ i ]  >ratings[i-1]) {
-				 candies [i ] = candies [i -1] +1 ;	 
-			 }
-				 
-		 }
-		 for  (int i = n-2 ; i >=0 ; i --) {
-			 if (ratings [i+1] > ratings [i ] ) {
-				 candies [i+1] = Math.max(candies [i] +1, candies[i+1]) ;
-				 
-				 System.out.println(  candies [i+1]  );//
-			 }
-		 }
-	 }
+
+	public  static int candy(int[] ratings) {
+//		https://leetcode.com/problems/candy/description/?envType=daily-question&envId=2023-09-13
+		int n = ratings.length;
+		int[] candies = new int[n];
+		Arrays.fill(candies, 1);
+
+		// left to right
+		for (int i = 1; i < n; i++) {
+			if (ratings[i] > ratings[i - 1]) {
+				candies[i] = candies[i - 1] + 1;
+				System.out.println(" A candies [i ]" + candies[i]);
+			}
+
+		}
+		for (int i = n - 2; i >= 0; i--) {
+			if (ratings[i] > ratings[i + 1]) {
+				candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+
+				System.out.println(" B candies [i]" + candies[i ]); // test
+
+			}
+		}
+		
+		int totalCandies = 0 ; 
+		for ( int  candy : candies) {
+			totalCandies += candy ;
+		}
+		return totalCandies ;
+
+	}
 }
