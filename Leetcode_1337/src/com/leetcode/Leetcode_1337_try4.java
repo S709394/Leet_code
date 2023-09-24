2572db14876fec1e2fefaf356c6126c77880feb0
Stephen <55069019+S709394@users.noreply.github.com>
2023-09-23 23:42:12 -0400

    Add files via upload

--- Leetcode_1337/src/com/leetcode/Leetcode_1337_try4.java
+++ Leetcode_1337/src/com/leetcode/Leetcode_1337_try4.java
@@ -13,22 +13,34 @@ public class Leetcode_1337_try4 {
 		int[] cntArr = new int[rows];
 		for (int i = 0; i < rows; i++) {
 			int curCnt = 0;
-
+			
 			for (int j = 0; j < cols; j++) {
 				if (mat[i][j] == 1) {
 					curCnt++;
 				}
 			}
 			cntArr[i] = curCnt;
-			System.out.println( "curCnt :" + curCnt ); //  test 
-			
-			 
+			System.out.println( "curCnt :" + curCnt ); //test
 		}
 		
+		int ans [] = new int [k] ;
 		for ( int i = 0 ; i < k ; i ++) {
+			int min =  Integer.MAX_VALUE;
 			
+			int minIdx = 0 ;
 			
+			for ( int j = 0 ; j < cntArr.length; j ++ ) {
+				if(  cntArr[i] <   min){
+					min = cntArr[i];
+					
+					minIdx = i ;
+				}
+			}			
+			ans [i] = cntArr [minIdx];
+			
+			cntArr[minIdx] = Integer.MAX_VALUE;
+			System.out.println( "cntArr[minIdx] :" + cntArr[minIdx] ); //test ;
 		}
-
+		 return ans ;
 	}
 }
