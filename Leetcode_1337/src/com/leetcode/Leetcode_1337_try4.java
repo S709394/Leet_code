994ae912c8964a9afcb8f75f0935a64351a8c136
Stephen <55069019+S709394@users.noreply.github.com>
2023-09-24 23:19:53 -0400

    1337_try4_debuging

--- Leetcode_1337/src/com/leetcode/Leetcode_1337_try4.java
+++ Leetcode_1337/src/com/leetcode/Leetcode_1337_try4.java
@@ -2,7 +2,11 @@ package com.leetcode;
 
 public class Leetcode_1337_try4 {
 	public static void main(String[] args) {
-
+		int[][] mat = { { 1, 1, 0, 0, 0 }, 
+				{ 1, 1, 1, 1, 0 }, 
+				{ 1, 0, 0, 0, 0 }, 
+				{ 1, 1, 0, 0, 0 },
+				{ 1, 1, 1, 1, 1 } };
 	}
 
 	public static int[] kWeakestRows(int[][] mat, int k) { // try4
@@ -13,34 +17,35 @@ public class Leetcode_1337_try4 {
 		int[] cntArr = new int[rows];
 		for (int i = 0; i < rows; i++) {
 			int curCnt = 0;
-			
+
 			for (int j = 0; j < cols; j++) {
 				if (mat[i][j] == 1) {
 					curCnt++;
 				}
 			}
 			cntArr[i] = curCnt;
-			System.out.println( "curCnt :" + curCnt ); //test
+			System.out.println("curCnt :" + curCnt); // test
 		}
-		
-		int ans [] = new int [k] ;
-		for ( int i = 0 ; i < k ; i ++) {
-			int min =  Integer.MAX_VALUE;
-			
-			int minIdx = 0 ;
-			
-			for ( int j = 0 ; j < cntArr.length; j ++ ) {
-				if(  cntArr[i] <   min){
-					min = cntArr[i];
-					
-					minIdx = i ;
+
+		int ans[] = new int[k];
+		for (int i = 0; i < k; i++) {
+			int min = Integer.MAX_VALUE;
+
+			int minIdx = 0;
+
+			for (int j = 0; j < cntArr.length; j++) {
+				if (cntArr[j] < min) {
+					min = cntArr[j];
+
+					minIdx = j;
+					System.out.println("minIdx : " + minIdx);// test
 				}
-			}			
-			ans [i] = cntArr [minIdx];
-			
+			}
+			ans[i] = minIdx;
+			System.out.println("cntArr[minIdx] :" + cntArr[minIdx]); // test ;
 			cntArr[minIdx] = Integer.MAX_VALUE;
-			System.out.println( "cntArr[minIdx] :" + cntArr[minIdx] ); //test ;
+
 		}
-		 return ans ;
+		return ans;
 	}
 }
