a7c56ad22ca31624b1e28daddfd3ef7d05912882
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-03 22:54:33 -0400

    Works well

--- Leetcode_880/src/com/leetcode/Leetcode_880_try1.java
+++ Leetcode_880/src/com/leetcode/Leetcode_880_try1.java
@@ -19,5 +19,34 @@ public class Leetcode_880_try1 {
 			 
 		}
 		
+		
+		// make sur k < size 
+		if ( k > size ) {
+			return null ;
+		}
+		
+		long sNo = size ; 
+		
+		
+		for ( int ix = chs.length -1 ;ix >=0 ; ix--) {
+				char ch = chs[ix] ;
+				// end situation 
+				if( k==0 && Character.isLetter(ch)) {
+					return String.valueOf(ch);
+				}
+				if(  Character.isDigit(ch)) {
+					int dgt =  (int) (ch-'0');
+					System.out.println( "dgt: " + dgt); //test
+					
+					sNo =sNo /dgt;
+					k%= sNo ; //*
+				}
+				else if ( k == sNo) {
+					return String.valueOf(ch);
+				}else {
+					sNo--;
+				}
+		}
+			return null;
 	}
 }
