abaa3293445ad576f224d91eaac2aa6da9a061f0
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-05 22:24:39 -0400

    Not works well

--- Leetcode_1512/src/com/leetcode/Leetcode_1512_try1.java
+++ Leetcode_1512/src/com/leetcode/Leetcode_1512_try1.java
@@ -3,6 +3,7 @@ package com.leetcode;
 public class Leetcode_1512_try1 {
 	public static void main(String[] args) {
 		int  []nums = { 1, 2, 3, 1, 1, 3 };
+		System.out.println( numIdenticalPairs (nums));
 	}
 
 	public static int numIdenticalPairs(int[] nums) {
@@ -11,15 +12,23 @@ public class Leetcode_1512_try1 {
 		 int left = 0 ; int right = len -1 ;
 		 
 		 int cnt  = 0 ;
-		  while (left < right ) {
-			  if (nums [ left ] == nums [right]) {
-				  cnt ++ ; 
-			  }
-		  }
+		
 		 
 		 
-//		 for (int i = 0 ; i < right ; i ++) {
-//			 
-//		 }
+		 for (int i = left ; i < right ; i ++) {
+			 System.out.println(" i : " + i);
+			 
+			 for ( int  j = i+1 ;  j < right ; j ++) {
+				 System.out.println("j :" + j );
+				 if( nums [ i] == nums[j]) {
+					 cnt ++ ;
+				 }
+			 }
+			 
+			 
+			 right --;
+			 
+		 }
+		 return cnt ;
 	}
 }
