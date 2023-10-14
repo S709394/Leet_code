7a0bc6a5ba3b061a6fecc5cc4791ab11eadd6721
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-13 23:58:44 -0400

    Add files via upload

--- Leetcode_557/src/com/leetcode/Leetcode_557_try4_no_stack_practise.java
+++ Leetcode_557/src/com/leetcode/Leetcode_557_try4_no_stack_practise.java
@@ -1,5 +1,47 @@
 package com.leetcode;
 
 public class Leetcode_557_try4_no_stack_practise {
+	public static void main(String[] args) {
+		String s = "let us go";
+		System.out.println(reverseWords(s));
 
+	}
+	public static String reverseWords(String s) { 
+		
+		//try 4 _ practise
+		char [] cArr = s.toCharArray();
+		
+		int len = s.length();
+		int l=0; 
+		int r=0;
+		for (int i = 0 ; i < len ; i++) {
+			
+			
+			if (cArr[i]!=' ') {
+				r++;
+			}else {
+				//or i-1?    *****
+				System.out.println( "r-1 :" + (r-1) );
+				reverse_C_Arr (l, r-1,cArr);
+				l=i+1;
+				r++;
+			}
+		}
+		reverse_C_Arr (l, len-1,cArr);
+		
+		return new String (cArr);
+		
+		
+	}
+	public static void reverse_C_Arr (int l , int r , char []cArr) {
+		char temp ;
+		while (l<r) {
+			temp = cArr[l];
+			cArr[l] = cArr[r];
+			cArr[r] = temp;
+			l++;
+			r--;
+		}
+		
+	}
 }
