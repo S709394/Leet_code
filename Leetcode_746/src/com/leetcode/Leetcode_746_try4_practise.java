5658a3269b7300d09f2eb4b005e1bdfdc4d5a7e9
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-15 20:33:27 -0400

    Try4 _workswell _ practise

--- Leetcode_746/src/com/leetcode/Leetcode_746_try4_practise.java
+++ Leetcode_746/src/com/leetcode/Leetcode_746_try4_practise.java
@@ -1,26 +1,24 @@
 package com.leetcode;
 
-public class Leetcode_746_try3_practise {
+public class Leetcode_746_try4_practise {
 	public static void main(String[] args) {
 		int[] cost = { 10, 15, 20 };
 		System.out.println(minCostClimbingStairs(cost));
 	}
 
-	public static int minCostClimbingStairs(int[] cost) {
-
-		// try3
-
+	public static int minCostClimbingStairs(int[] cost) { 
+		
+		//try4 
 		int len = cost.length;
-
-		int[] dp = new int[len + 1];
-
+		int[] dp = new int[len];
 		dp[0] = 0;
 		dp[1] = 0;
-
 		for (int i = 2; i < len; i++) {
-			dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+			dp[i] = Math.max(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+
 		}
 
 		return Math.min(dp[len - 2] + cost[len - 2], dp[len - 1] + cost[len - 1]);
 	}
+
 }
