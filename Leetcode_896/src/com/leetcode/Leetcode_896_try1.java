7de241b78ca4aa1a591e3eb19e76bcb013bc503d
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-17 23:05:17 -0400

    Not sure works or not

---
+++ Leetcode_896/src/com/leetcode/Leetcode_896_try1.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+public class Leetcode_896_try1 {
+	public static void main(String[] args) {
+		int nums[] = { 1, 2, 2, 3 };
+		System.out.println(isMonotonic(nums));
+	}
+
+	public static boolean isMonotonic(int[] nums) {
+
+		// try1
+		int len = nums.length;
+
+		int left = 0;
+//			int	right = len - 1;
+
+		while (nums[left] == nums[left + 1] && left < len - 1) {
+			left++;
+		}
+
+		if (nums[left] < nums[left + 1]) {
+//			boolean ans = true;
+			for (int i = 1; i < len - 1; i++) {
+//				System.out.println(i);
+				if (nums[i] >= nums[i + 1]) {
+					return false;
+				}
+
+			}
+
+		} else {
+			for (int i = 1; i < len - 1; i++) {
+				if (nums[i] <= nums[i + 1]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
