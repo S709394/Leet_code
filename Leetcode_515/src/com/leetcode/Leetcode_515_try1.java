37ed178a5d0faa7f680d5b45d98132535cba16c3
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-25 00:08:56 -0400

    515_

---
+++ Leetcode_515/src/com/leetcode/Leetcode_515_try1.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class Leetcode_515_try1 {
+	// https://leetcode.com/problems/find-largest-value-in-each-tree-row/solutions/1368671/java-easy-approach-with-expanation-recursive-preorder/?envType=daily-question&envId=2023-10-24d
+
+	private List<Integer> ansList = new ArrayList<>();
+
+	public List < Integer> largestValues (TreeNode root ) {
+	
+	
+	}
