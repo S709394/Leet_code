a674de15a5c921eda9629efef9ef005cb18e6efe
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-27 22:59:59 -0400

    Going to practise _Oct _29    ( try6

--- Leetcode_20/src/com/leetcode/Leetcode_20_try5_practise.java
+++ Leetcode_20/src/com/leetcode/Leetcode_20_try5_practise.java
@@ -3,26 +3,40 @@ package com.leetcode;
 import java.util.Stack;
 
 public class Leetcode_20_try5_practise {
-	public class Leetcode_20_try1 {
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 //			String s = "(){}";
 //			String s = "})}";
 
-			String s = "({()})";
+		String s = "({()})";
 //			String s1 = "({))";
 
-			boolean b = isVaild(s);
-			System.out.println(b);
+		boolean b = isVaild(s);
+		System.out.println(b);
 
-			System.out.println("b1");
+		System.out.println("b1");
 //			boolean b1 = isVaild (s1);
 //			System.out.println(b1);
 
 //			String s3 = "({))";
-		}
+	}
 
-		public static boolean isVaild (String s ) {
-			
+	public static boolean isVaild(String s) {
+		Stack<Character> stack = new Stack<>();
+
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				stack.add(')');
+			} else if (c == '{') {
+				stack.add('}');
+			} else if (c == '[') {
+				stack.add(']');
+//				}else if ( !stack .isEmpty() && c!= stack.pop() ) {  //wrong 
+				// left : " ]" "a" right "[}" "(a"
+			} else if (stack.isEmpty() || c != stack.pop()) {
+				return false;
+			}
 		}
+		return stack.isEmpty();
+	}
 }
---
+++ Leetcode_20/src/com/leetcode/Leetcode_try6_practise.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+public class Leetcode_try6_practise {
+	public static void main(String[] args) {
+//		String s = "(){}";
+//		String s = "})}";
+
+		String s = "({()})";
+//		String s1 = "({))";
+
+		boolean b = isVaild(s);
+		System.out.println(b);
+
+		System.out.println("b1");
+//		boolean b1 = isVaild (s1);
+//		System.out.println(b1);
+
+//		String s3 = "({))";
+	}
+
+	public static boolean isVaild(String s) {
+
+	}
+}
