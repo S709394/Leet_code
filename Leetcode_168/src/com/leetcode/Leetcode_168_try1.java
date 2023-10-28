ef376af0cd04f797fa6786fe027cd0c8a12aae97
Stephen <55069019+S709394@users.noreply.github.com>
2023-10-27 23:08:57 -0400

    Practise in the 3rd day _168_try1

---
+++ Leetcode_168/src/com/leetcode/Leetcode_168_try1.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+public class Leetcode_168_try1 {
+	public static void main(String[] args) {
+		int columnNumber = 1;
+
+	}
+
+	public static  String convertToTitle(int columnNumber) {
+
+	}
+
+}
