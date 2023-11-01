c5bf1a9e581833050d49edbeb445456e7638dd27
Stephen <55069019+S709394@users.noreply.github.com>
2023-11-01 13:25:52 -0400

    501_inorder  unfinished

---
+++ Leetcode_501/src/com/leetcode/Leetcode_501_try1_inorder.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+public class Leetcode_501_try1_inorder {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public int[] findMode(TreeNode root) {
+
+	}
+}
