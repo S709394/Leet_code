7a17c9aa5e26b336c792fef84705ae0ff7ab173c
Stephen <55069019+S709394@users.noreply.github.com>
2023-11-05 21:25:57 -0500

    501_try3_works
    https://leetcode.com/problems/find-mode-in-binary-search-tree/solutions/98100/java-4ms-beats-100-extra-o-1-solution-no-map/

--- Leetcode_501/src/com/leetcode/Leetcode_501_try1_inorder.java
+++ Leetcode_501/src/com/leetcode/Leetcode_501_try1_inorder.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode_501_try1_inorder {
+	
+	//works in try3
+	//https://leetcode.com/problems/find-mode-in-binary-search-tree/solutions/98100/java-4ms-beats-100-extra-o-1-solution-no-map/
 	public class TreeNode {
 		int val;
 		TreeNode left;
@@ -23,7 +26,7 @@ public class Leetcode_501_try1_inorder {
 		}
 	}
 
-	static TreeNode prev = null;
+	static Integer prev = null;
 	static int count = 1;
 	static int max = 0;
 
@@ -36,6 +39,12 @@ public class Leetcode_501_try1_inorder {
 
 		traverse(root, modes);
 
+		int[] res = new int[modes.size()];
+
+		for (int i = 0; i < modes.size(); i++) {
+			res[i] = modes.get(i);
+		}
+		return res;
 	}
 
 	public static void traverse(TreeNode root, List<Integer> modes) {
@@ -43,24 +52,22 @@ public class Leetcode_501_try1_inorder {
 			return;
 		}
 		traverse(root.left, modes);
-		if (root.val == prev.val) {
-			count ++ ;
-		}else {
-			count --;
+		if (prev!= null) {
+			count++;
+		} else {
+			count--;
 		}
-		
-		if ( count > max ) {
-			max=count ;
+
+		if (count > max) {
+			max = count;
 			modes.clear();
 			modes.add(root.val);
-				
-		}else if (count ==max) {
+
+		} else if (count == max) {
 			modes.add(root.val);
 		}
-		prev = root;
+		prev = root.val;
 		traverse(root.right, modes);
-
-	
 
 	}
 }
