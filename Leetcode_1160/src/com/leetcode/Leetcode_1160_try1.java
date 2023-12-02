2c9081edbd3e202f81482b97ffeb459097769e87
Stephen <55069019+S709394@users.noreply.github.com>
2023-12-01 23:33:59 -0500

    An easy question , tring

---
+++ Leetcode_1160/src/com/leetcode/Leetcode_1160_try1.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+public class Leetcode_1160_try1 {
+	public static void main(String[] args) {
+
+	}
+
+	public static  int countCharacters(String[] words, String chars) {
+//	try1
+
+	}
+}
