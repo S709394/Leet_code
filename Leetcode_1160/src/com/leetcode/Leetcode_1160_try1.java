75bc30556378c5d891208f740dde64867d831374
Stephen <55069019+S709394@users.noreply.github.com>
2023-12-03 15:38:14 -0500

    It works well

--- Leetcode_1160/src/com/leetcode/Leetcode_1160_try1.java
+++ Leetcode_1160/src/com/leetcode/Leetcode_1160_try1.java
@@ -1,12 +1,48 @@
 package com.leetcode;
 
+import java.util.Arrays;
+
 public class Leetcode_1160_try1 {
 	public static void main(String[] args) {
-
+		String[] words = { "cat", "bt", "hat", "tree" };
+		String chars = "atach";
+		System.out.println(countCharacters(words, chars));
 	}
 
-	public static  int countCharacters(String[] words, String chars) {
-//	try1
+	public static int countCharacters(String[] words, String chars) {
+//	try1	
+		int[] cnt = new int[26];
 
+		for (char c : chars.toCharArray()) {
+			cnt[c - 'a']++;
+		}
+
+		int ans = 0;
+		for (String word : words) {
+			int len = word.length();
+			if (wordInChars(cnt, word)) {
+				ans += len;
+			}
+
+		}
+		return ans;
+	}
+
+	public static boolean wordInChars(int[] cntArr, String word) {
+
+//		int [] cntCopy  = new Arrays .copyOf(null, 0) ;
+
+		int[] cntCopy = new int[26];
+
+		for (int i = 0; i < 26; i++) {
+			cntCopy[i] = cntArr[i];
+		}
+		for (char c : word.toCharArray()) {
+			cntCopy[c - 'a']--;
+			if (cntCopy[c - 'a'] < 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
