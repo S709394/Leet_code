cc24f66cb4ac6a13c9dba86d7d2fd49e7c08826d
Stephen <55069019+S709394@users.noreply.github.com>
2023-12-05 20:44:10 -0500

    Add files via upload

---
+++ Leetcode_2264/src/com/leetcode/Leetcode_2264_try1.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+public class Leetcode_2264_try1 {
+	public String largestGoodInteger(String num) {
+		for (int i = 2; i < num.length(); i++) {
+			//create a '3 in a row' str
+
+			// compare the every 3 subStr
+			String subStr = num.substring(i - 2, i);
+
+		}
+
+	}
+
+	// find the biggest , maybe 999
+	public int getNum(String subStr) {
+
+	}
+}
