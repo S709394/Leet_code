c7f2ab93e3f7cc33c11e4bb0642c083b169b2271
Stephen <55069019+S709394@users.noreply.github.com>
2023-12-07 22:35:24 -0500

    Try1

--- Leetcode_2264/src/com/leetcode/Leetcode_2264_try1.java
+++ Leetcode_2264/src/com/leetcode/Leetcode_2264_try1.java
@@ -3,10 +3,13 @@ package com.leetcode;
 public class Leetcode_2264_try1 {
 	public String largestGoodInteger(String num) {
 		for (int i = 2; i < num.length(); i++) {
-			//create a '3 in a row' str
+			// create a '3 in a row' str
 
-			// compare the every 3 subStr
+			// make sure all char s in this len 3 subString is the same
 			String subStr = num.substring(i - 2, i);
+			if (num.charAt(i) == num.charAt(i - 1) && num.charAt(i - 1) == num.charAt(i - 2)) {
+
+			}
 
 		}
 
