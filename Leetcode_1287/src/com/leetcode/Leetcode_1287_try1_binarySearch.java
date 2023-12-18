919935d46104fc2911dd25827c46c080495d4ef4
Stephen <55069019+S709394@users.noreply.github.com>
2023-12-17 23:54:14 -0500

    1. if length == 1

--- Leetcode_1287/src/com/leetcode/Leetcode_1287_try1_binarySearch.java
+++ Leetcode_1287/src/com/leetcode/Leetcode_1287_try1_binarySearch.java
@@ -1,5 +1,14 @@
 package com.leetcode;
 
 public class Leetcode_1287_try1_binarySearch {
+	public static void main(String[] args) {
 
+	}
+
+	// https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/solutions/614659/java-binary-search-with-o-log-n-solution/?envType=daily-question&envId=2023-12-11
+	public int findSpecialInteger(int[] arr) {
+		if (arr.length == 1) {
+			return arr[0];
+		}
+	}
 }
